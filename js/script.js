225f3634c9c38b06a24214902b8f92f9e57c55bb
Lucia Santellan <lusantellan@gmail.com>
2023-03-14 22:38:08 -0300

    modif find

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ class Servicio {
   }
 
 asignarId(array){
-  this.id= array.length +1;
+  this.id= array.length;
 }
 }
 
@@ -17,62 +17,27 @@ const servicios = [
   new Servicio('Estetica de pies', 750, 4),
 ]
 
-/* function buscarServicio(nombreServicio){
-   const servicioEncontrado = servicios.find(servicio => servicio.nombre === nombreServicio);
-   return servicioEncontrado;
- }
-
- const nombreServicio = prompt ('Ingresa el nombre del servicio que deseas buscar');
- const servicioEncontrado =  buscarServicio(nombreServicio);
-
- if (servicioEncontrado) {
-   alert(`Gracias por seleccionar ${servicioEncontrado.nombre} El precio del servicio es de $${servicioEncontrado.precio}`);
- } else {
-   alert('Por favor ingresa un nombre de servicio válido');
- }*/
-
-
-
-do {
-  let ingresoServicio = parseInt(prompt('Ingresa la opción de servicio que deseas contratar'));
+ do {
+  servicio = parseInt(prompt('Ingresa la opción de servicio que deseas contratar'));
   serviciosPermitidos = 4;
   
-    if (isNaN(ingresoServicio)) {
-      alert('Debe ingresar un número');
-    } else if (ingresoServicio > serviciosPermitidos) {
-      alert('Debe ingresar un número de servicio válido');
-    } else if (ingresoServicio == 1) {
-        alert('Gracias por seleccionar Uñas esculpidas. El precio del servicio es de $1.300')
-        /*function agregalAlcarrito(servicio1){
-          carrito.push(servicio1);
-        }*/
-      } else if (ingresoServicio == 2) {
-        alert('Gracias por seleccionar Esmaltado semipermanente. El precio del servicio es de $800')
-      } else if (ingresoServicio == 3) {
-        alert('Gracias por seleccionar Kapping. el precio del servicio es de $800')
-      } else if (ingresoServicio == 4) {
-        alert('Gracias por seleccionar Estética de pies. El precio del servicio es de $750')
-      }
-    } while (isNaN(ingresoServicio) || ingresoServicio > serviciosPermitidos);
+  if (isNaN(servicio)) {
+    alert('Debe ingresar un número');
+  } else if (servicio > serviciosPermitidos) {
+    alert('Por favor ingrese un número de servicio válido');
+  } 
+  const buscarServicio = servicios.find(serv => serv.id === ingresoServicio)
+  console.log('servicio encontrado');
+  
+   if (servicioEncontrado) {
+     alert(`Gracias por seleccionar ${servicioEncontrado.nombre} El precio del servicio es de $${servicioEncontrado.precio}`);
+   } else {
+     alert('Por favor ingresa un nombre de servicio válido');
+   }
+  } while (isNaN(servicio) || servicio > serviciosPermitidos);
 
-/*const carritoDeCompras = {
-  items: []
-}
-let total = 0;
-for (let i = 0; i < carritoDeCompras.length; i++){
-  total += carritoDeCompras[i].precio;
-}
-console.log("Carrito de compras:");
-for (let i = 0; i < carritoDeCompras.length; i++) {
-  console.log("-" + carritoDeCompras[i].nombre + " ($" + carritoDeCompras[i].precio + ")");
-}
-console.log ("Total: $" + total);
-let mensaje = "Carrito de compras: ";
-for (let i = 0; i < carritoDeCompras.length; i++){
-  mensaje += "- " + carritoDeCompras[i].nombre + " ($" + carritoDeCompras[i].precio + ")/n";
-}
-mensaje += "Total: $" + total;
-alert(mensaje);*/
+
+
 
 
 
